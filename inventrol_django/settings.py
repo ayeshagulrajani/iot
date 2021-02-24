c266267fb9e3d74fee0d2f02ae72ac80864f98d5
Ayesha Gulrajani <ayesha@ubuntu1804.linuxvmimages.local>
2021-02-24 04:07:00 -0500

    Jenkins commit

--- inventrol_django/settings.py
+++ inventrol_django/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'inventrol_django.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,6 +95,14 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
         },
     }
+}
+'''
+
+DATABASES = {
+	"default" : {
+		"ENGINE" : "django.db.backends.sqlite3",
+		"NAME" : "db.sqlite3"
+}
 }
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
